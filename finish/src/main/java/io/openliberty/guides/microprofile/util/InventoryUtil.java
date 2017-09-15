36ba7e54a6dec420b4b970fe4bc7829043625ea4
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2017-09-15 17:43:02 -0400

    merged rest and microprofile into a single app + issue 17

---
+++ finish/src/main/java/io/openliberty/guides/microprofile/util/InventoryUtil.java
@@ -0,0 +1,88 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::comment[]
+package io.openliberty.guides.microprofile.util;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class InventoryUtil {
+
+    private static final int PORT = 9080;
+    private static final String PROTOCOL = "http";
+    private static final String SYSTEM_PROPERTIES = "/system/properties";
+
+    public static JsonObject getProperties(String hostname) {
+        Client client = ClientBuilder.newClient();
+        URI propURI = InventoryUtil.buildUri(hostname);
+        return client.target(propURI).request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+    }
+    
+    // tag::buildHostJson[]
+    public static JsonObject buildHostJson(String hostname, String url) {
+        return Json.createObjectBuilder()
+                   .add("hostname", hostname)
+                   .add("_links", InventoryUtil.buildLinksForHost(hostname, url))
+                   .build();
+    }
+    // end::buildHostJson[]
+    
+    // tag::buildLinksForHost[]
+    public static JsonArray buildLinksForHost(String hostname, String invUri) {
+        
+        JsonArrayBuilder links = Json.createArrayBuilder(); 
+        
+        links.add(Json.createObjectBuilder()
+                      .add("href", StringUtils.appendIfMissing(invUri, "/") + hostname)
+                      .add("rel", "self"));
+        
+        links.add(Json.createObjectBuilder()
+                .add("href", InventoryUtil.buildUri(hostname).toString())
+                .add("rel", "properties"));
+        
+        return links.build();
+    }
+    // end::buildLinksForHost[]
+    
+    public static boolean responseOk(String hostname) {
+        try {
+            URL target = new URL(buildUri(hostname).toString());
+            HttpURLConnection http = (HttpURLConnection) target.openConnection();
+            http.setConnectTimeout(50);
+            int response = http.getResponseCode();
+            return (response != 200) ? false : true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static URI buildUri(String hostname) {
+        return UriBuilder.fromUri(SYSTEM_PROPERTIES)
+                .host(hostname)
+                .port(PORT)
+                .scheme(PROTOCOL)
+                .build();
+    }
+
+}
---
+++ finish/src/test/java/it/io/openliberty/guides/hateoas/EndpointTest.java
@@ -0,0 +1,168 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::comment[]
+package it.io.openliberty.guides.hateoas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.json.JsonArray;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
+
+// tag::class[]
+public class EndpointTest {
+    // tag::class-contents[]
+    // tag::setup[]
+    private String port;
+    private String baseUrl;
+    
+    private Client client;
+    
+    private final String SYSTEM_PROPERTIES = "system/properties";
+    private final String INVENTORY_HOSTS = "inventory/hosts";
+    
+    @Before
+    public void setup() {
+        // tag::urlCreation[]
+        port = System.getProperty("liberty.test.port");
+        baseUrl = "http://localhost:" + port + "/";
+        // end::urlCreation[]
+        
+        // tag::clientInit[]
+        client = ClientBuilder.newClient();
+        client.register(JsrJsonpProvider.class);
+        // end::clientInit[]
+    }
+    
+    @After
+    public void teardown() {
+        client.close();
+    }
+    // end::setup[]
+    
+    // tag::testSuite[]
+    @Test
+    public void testSuite() {
+        this.testLinkForInventoryContents();
+        this.testLinksForSystem();
+    }
+    // end::testSuite[]
+    
+    /**
+     * Checks if the HATEOAS link for the inventory contents (hostname=*) is as expected.
+     */
+    // tag::testLinkForInventoryContents[]
+    public void testLinkForInventoryContents() {
+        Response response = this.getResponse(baseUrl + INVENTORY_HOSTS);
+        this.assertResponse(baseUrl, response);
+        
+        // tag::jsonobj[]
+        JsonArray sysArray = response.readEntity(JsonArray.class);
+        // end::jsonobj[]
+        
+        // tag::assertAndClose[]
+        String expected, actual;
+
+        expected = baseUrl + INVENTORY_HOSTS + "/*";
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(0).getString("href");
+        assertEquals("Incorrect href", expected, actual);
+        
+        // asserting that rel was correct
+        expected = "self";
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(0).getString("rel");
+        assertEquals("Incorrect rel", expected, actual);
+        
+        response.close();
+        // end::assertAndClose[]
+    }
+    // end::testLinkForInventoryContents[]
+    
+    /**
+     * Checks that the HATEOAS links, with relationships 'self' and 'properties' for a simple 
+     * localhost system is as expected.
+     */
+    // tag::testLinksForSystem[]
+    public void testLinksForSystem() {
+        this.visitLocalhost();
+        
+        Response response = this.getResponse(baseUrl + INVENTORY_HOSTS);
+        this.assertResponse(baseUrl, response);
+        
+        JsonArray sysArray = response.readEntity(JsonArray.class);
+        
+        String expected, actual;
+        
+        // testing the 'self' link
+
+        expected = baseUrl + INVENTORY_HOSTS + "/localhost";
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(0).getString("href");
+        assertEquals("Incorrect href", expected, actual);
+        
+        expected = "self";
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(0).getString("rel");
+        assertEquals("Incorrect rel", expected, actual);
+        
+        // testing the 'properties' link
+        
+        expected = baseUrl + SYSTEM_PROPERTIES;
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(1).getString("href");
+        assertEquals("Incorrect href", expected, actual);
+        
+        expected = "properties";
+        actual = sysArray.getJsonObject(0).getJsonArray("_links").getJsonObject(1).getString("rel");
+        assertEquals("Incorrect rel", expected, actual);
+    }
+    // end::testLinksForSystem[]
+    
+    /**
+     * Returns a Response object for the specified URL.
+     */
+    // tag::getResponse[]
+    private Response getResponse(String url) {
+        return client.target(url).request().get();
+    }
+    // end::getResponse[]
+    
+    /**
+     * Asserts that the given URL has the correct (200) response code.
+     */
+    // tag::assertResponse[]
+    private void assertResponse(String url, Response response) {
+        assertEquals("Incorrect response code from " + url, 200, response.getStatus());;
+    }
+    // end::assertResponse[]
+    
+    /**
+     * Makes a GET request to localhost at the Inventory service.
+     */
+    // tag::visitLocalhost[]
+    private void visitLocalhost() {
+        Response response = this.getResponse(baseUrl + SYSTEM_PROPERTIES);
+        this.assertResponse(baseUrl, response);
+        response.close();
+            
+        Response targetResponse = client.target(baseUrl + INVENTORY_HOSTS + "/localhost")
+                                        .request()
+                                        .get();
+        targetResponse.close();
+    }
+    // end::visitLocalhost[]
+    // end::class-contents[]
+}
+// end::class[]
