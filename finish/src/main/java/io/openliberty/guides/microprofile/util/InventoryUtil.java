9c7c7484216384bbf0850806ac9e391416de3770
Gilbert Kwan <gkwan@ca.ibm.com>
2019-02-20 17:32:48 -0500

    Update InventoryUtil.java

--- finish/src/main/java/io/openliberty/guides/microprofile/util/InventoryUtil.java
+++ finish/src/main/java/io/openliberty/guides/microprofile/util/InventoryUtil.java
@@ -1,6 +1,6 @@
 // tag::comment[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,9 @@ public class InventoryUtil {
     public static JsonObject getProperties(String hostname) {
         Client client = ClientBuilder.newClient();
         URI propURI = InventoryUtil.buildUri(hostname);
-        return client.target(propURI).request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+        return client.target(propURI)
+                     .request(MediaType.APPLICATION_JSON)
+                     .get(JsonObject.class);
     }
     
     // tag::buildHostJson[]
