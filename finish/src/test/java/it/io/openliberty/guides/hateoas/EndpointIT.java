e7dd792c423739dfbe04053febb3e0796ff84f11
Kartik Saravana Arvind <kartiksaravanaibm.com@kartiks-mbp.war.can.ibm.com>
2020-01-22 15:33:03 -0500

    simplified pom; updated test to have IT in its name; migrated test to junit5

--- finish/src/test/java/it/io/openliberty/guides/hateoas/EndpointIT.java
+++ finish/src/test/java/it/io/openliberty/guides/hateoas/EndpointIT.java
@@ -12,12 +12,6 @@
  // end::comment[]
 package it.io.openliberty.guides.hateoas;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import javax.json.JsonArray;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -25,8 +19,14 @@ import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // tag::class[]
-public class EndpointTest {
+public class EndpointIT {
     // tag::class-contents[]
     // tag::setup[]
     private String port;
@@ -38,7 +38,7 @@ public class EndpointTest {
     private final String INVENTORY_HOSTS = "inventory/hosts";
     
     // tag::Before[]
-    @Before
+    @BeforeEach
     // end::Before[]
     public void setup() {
         // tag::urlCreation[]
@@ -53,27 +53,18 @@ public class EndpointTest {
     }
     
     // tag::After[]
-    @After
+    @AfterEach
     // end::After[]
     public void teardown() {
         client.close();
     }
     // end::setup[]
     
-    // tag::Test[]
-    @Test
-    // end::Test[]
-    // tag::testSuite[]
-    public void testSuite() {
-        this.testLinkForInventoryContents();
-        this.testLinksForSystem();
-    }
-    // end::testSuite[]
-    
     /**
      * Checks if the HATEOAS link for the inventory contents (hostname=*) is as expected.
      */
     // tag::testLinkForInventoryContents[]
+    @Test
     public void testLinkForInventoryContents() {
         Response response = this.getResponse(baseUrl + INVENTORY_HOSTS);
         this.assertResponse(baseUrl, response);
@@ -89,12 +80,12 @@ public class EndpointTest {
 
         expected = baseUrl + INVENTORY_HOSTS + "/*";
         actual = links.getJsonObject(0).getString("href");
-        assertEquals("Incorrect href", expected, actual);
+        assertEquals(expected, actual, "Incorrect href");
         
         // asserting that rel was correct
         expected = "self";
         actual = links.getJsonObject(0).getString("rel");
-        assertEquals("Incorrect rel", expected, actual);
+        assertEquals(expected, actual, "Incorrect rel");
         
         response.close();
         // end::assertAndClose[]
@@ -106,6 +97,7 @@ public class EndpointTest {
      * localhost system is as expected.
      */
     // tag::testLinksForSystem[]
+    @Test
     public void testLinksForSystem() {
         this.visitLocalhost();
         
@@ -119,24 +111,22 @@ public class EndpointTest {
         JsonArray links = sysArray.getJsonObject(0).getJsonArray("_links");
         
         // testing the 'self' link
-
         expected = baseUrl + INVENTORY_HOSTS + "/localhost";
         actual = links.getJsonObject(0).getString("href");
-        assertEquals("Incorrect href", expected, actual);
+        assertEquals(expected, actual, "Incorrect href");
         
         expected = "self";
         actual = links.getJsonObject(0).getString("rel");
-        assertEquals("Incorrect rel", expected, actual);
+        assertEquals(expected, actual, "Incorrect rel");
         
         // testing the 'properties' link
-        
         expected = baseUrl + SYSTEM_PROPERTIES;
         actual = links.getJsonObject(1).getString("href");
-        assertEquals("Incorrect href", expected, actual);
+        assertEquals(expected, actual, "Incorrect href");
         
         expected = "properties";
         actual = links.getJsonObject(1).getString("rel");
-        assertEquals("Incorrect rel", expected, actual);
+        assertEquals(expected, actual, "Incorrect rel");
     }
     // end::testLinksForSystem[]
     
@@ -154,7 +144,7 @@ public class EndpointTest {
      */
     // tag::assertResponse[]
     private void assertResponse(String url, Response response) {
-        assertEquals("Incorrect response code from " + url, 200, response.getStatus());;
+        assertEquals(200, response.getStatus(), "Incorrect response code from " + url);;
     }
     // end::assertResponse[]
     
